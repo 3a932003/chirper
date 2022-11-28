97d8070bd3ed659be9f9b8f603101293d5bcb2db
xu qian su <s3a932003@student.ncut.edu.tw>
2022-11-28 22:16:31 +0800

    Navigation menu

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -11,6 +11,22 @@
             </textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                 <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                     {{ __('Dashboard') }}
+                 </x-nav-link>
+                 <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                     {{ __('Chirps') }}
+                 </x-nav-link>
+             </div>
+             <div class="pt-2 pb-3 space-y-1">
+                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                     {{ __('Dashboard') }}
+                 </x-responsive-nav-link>
+                 <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                     {{ __('Chirps') }}
+                 </x-responsive-nav-link>
+             </div>
         </form>
     </div>
 </x-app-layout>
